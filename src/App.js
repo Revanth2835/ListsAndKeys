f2e4ebe8404c1fea5c65e95a6174cc7df447ea67
Revanth2835 <revanthkumar2835@gmail.com>
2025-09-30 22:12:58 +0530

    KeysAndLists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import UserProfile from './Components/UserProfile/index'
+import './App.css'
+const UserDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Revanth',
+    role: 'Data Analyst',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Thanya',
+    role: 'Data Engineer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Daniel',
+    role: 'Data Scientist',
+  },
+]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// const App = () => <UserProfile UserDetails={UserDetailsList} />
+const App = () => (
+  <div className="list-container">
+    <h1 className="title"> Users List</h1>
+    <ul>
+      {UserDetailsList.map(each => (
+        <UserProfile UserDetails={each} key={each.uniqueNo} />
+      ))}
+    </ul>
+  </div>
+)
 
-export default App;
+export default App
